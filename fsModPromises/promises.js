2f0a2923644bbd428f342fd68292e04234912d79
CoolShivas <shivas2710cool00@gmail.com>
2025-03-10 19:04:44 +0530

    Use of fs.promises.readdir() to Read the Directory and its files.

---
+++ fsModPromises/promises.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const files = __dirname;
+
+const readDirFiles = fs.promises
+  .readdir(files)
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
+console.log(readDirFiles);
+
+// // // // Here, we are getting the Output on Terminal as :
+// // // // Promise { <pending> }
+// // // // [ 'promises.js', 'theory.js' ]
+// // // // Therefore, it is showing that how many files are there in your directory i.e., the folder names as fsModPromises and its files. So, it is showing the file promise.js and theory.js.
+
+
